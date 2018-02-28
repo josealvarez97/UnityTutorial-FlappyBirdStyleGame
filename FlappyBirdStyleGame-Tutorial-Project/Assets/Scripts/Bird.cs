a6bea2590778248b3903b2ae818866e7b9622505
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-02-28 01:16:36 -0800

    Added script for controlling the Bird
    Based on the official tutorials from the Uninty documentation. Link: https://unity3d.com/learn/tutorials/topics/2d-game-creation/bird-script?playlist=17093
    
    - Added code for exerting a force on Bird's rigid body based on input from the player and the condition that the bird has not collieded with another collider during playtime.
    - Interesting technique: Reseting velocity vector (to vector.zero) of the object in order to make the application of the force seem "the same everytime"

---
+++ FlappyBirdStyleGame-Tutorial-Project/Assets/Scripts/Bird.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public float forceMagnitude;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2dReference;
+
+	// Use this for initialization
+	void Start () {
+
+        rb2dReference = GetComponent<Rigidbody2D>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (!isDead)
+        {
+            if (Input.GetMouseButtonDown(0)) // For the left mouse button 
+            {
+                //(PROBABLY NOT THE BEST WAY TO TAKE USER INPUT...) 
+                //AXIS ARE BETTER IN CASE USER WANTS TO CHANGE INPUT CONFIGURATIONS...
+
+
+                rb2dReference.velocity = Vector2.zero; // Shorthand writing for new Vector2(0,0). //For replicating the same behavior each time the players exerts a force (to avoid making it additive).
+                Vector2 forceVector = new Vector2(0, 1);
+                rb2dReference.AddForce(forceVector * forceMagnitude);
+            }
+        }
+
+	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+    }
+
+
+
+}
