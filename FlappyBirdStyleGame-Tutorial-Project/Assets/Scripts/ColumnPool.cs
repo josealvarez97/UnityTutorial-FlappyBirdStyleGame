02cdd2d3239c314add3cc1ae5a5e9c8e3b2319f0
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-03-03 12:41:07 -0800

    FINISHED TUTORIAL!!! Created object pool of columns through object pool pattern.
    Based on the official tutorials from the Unity documentation. Link: https://unity3d.com/learn/tutorials/topics/2d-game-creation/recycling-obstacles-object-pooling?playlist=17093
    
    * Learned to create performant infinite or endless style games by moving the world instead of the player.
    * Learned to optimize game by using object pooling to create the illusion of many objects using only a few.
    
    Learned to create a pool of objects that I can reuse. This improves performance since it avoids having to instantiate and destroy objects infinitely.
    Useful documentation:
    - Wikipidia - Object Pool Pattern (https://en.wikipedia.org/wiki/Object_pool_pattern)
    - Ray Wenderlich - Object Pooling in Unity (https://www.raywenderlich.com/136091/object-pooling-unity)
    -  Object Pooling  (https://unity3d.com/learn/tutorials/topics/scripting/object-pooling)

---
+++ FlappyBirdStyleGame-Tutorial-Project/Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour {
+
+    public int columnsPoolSize = 5;
+    public GameObject columnsPrefab;
+    public float spawnRate = 4f;
+    public float columnMin = -1f;
+    public float columnMax = 3.5f;
+
+    private GameObject[] columns;
+    private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+    private float timeSinceLastSpawned;
+    private float spawnXPosition = 10f;
+    private int currentColumn = 0;
+
+
+
+	// Use this for initialization
+	void Start () {
+        columns = new GameObject[columnsPoolSize];
+
+        for (int i = 0; i < columnsPoolSize; i++)
+        {
+            columns[i] = GameObject.Instantiate(columnsPrefab, objectPoolPosition, Quaternion.identity);
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeSinceLastSpawned += Time.deltaTime;
+        
+        if (!GameControl.instance.gameOver && timeSinceLastSpawned >= spawnRate)
+        {
+            timeSinceLastSpawned = 0;
+            float spawnYPosition = Random.Range(columnMin, columnMax);
+
+            columns[currentColumn].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+            // beautiful understandable intuitive code
+            currentColumn++;
+
+            if (currentColumn >= columnsPoolSize)
+                currentColumn = 0;
+        }
+	}
+}
